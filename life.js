295fe34b2cdfef3c4536d91c80b0f7ab166e912a
Hammed Abdullah Ishola <abdullahishola57@gmail.com>
2022-06-22 09:50:43 -0700

    Javascript Calaculator

--- life.js
+++ life.js
@@ -1,25 +1,25 @@
 let operator=window.prompt("Enter Operator: +, -, *, /")
 if(operator=="+"){
-   let num1=window.prompt("Enter First Number: ")
-   let num2=window.prompt("Enter Second Number: ")
+   let num1=parseFloat(prompt("Enter First Number: "))
+   let num2=parseFloat(prompt("Enter Second Number: "))
    let result=num1+num2
     alert(result)
 }
 else if(operator=="-"){
-   let num1=window.prompt("Enter First Number: ")
-   let num2=window.prompt("Enter Second Number: ")
+   let num1=parseFloat(prompt("Enter First Number: "))
+   let num2=parseFloat(prompt("Enter Second Number: "))
    let result=num1-num2
     alert(result)
 }
 else if(operator=="*"){
-    let num1=window.prompt("Enter First Number: ")
-   let  num2=window.prompt("Enter Second Number: ")
+    let num1=parseFloat(prompt("Enter First Number: "))
+   let num2=parseFloat(prompt("Enter Second Number: "))
    let  result=num1*num2
     alert(result)
 }
 else if(operator=="/"){
-    let num1=window.prompt("Enter First Number: ")
-    let num2=window.prompt("Enter Second Number: ")
+    let num1=parseFloat(prompt("Enter First Number: "))
+   let num2=parseFloat(prompt("Enter Second Number: "))
     let result=num1/num2
     alert(result)
 }
